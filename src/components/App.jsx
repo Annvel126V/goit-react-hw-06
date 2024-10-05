5dd31b8bc2d48497737b3da3ac5842df539bf5d0
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-10-05 13:20:17 +0200

    add icon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,15 @@ import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
 import SearchBox from "./SearchBox/SearchBox";
 import s from "./App.module.css";
+import { FaPhoneVolume } from "react-icons/fa6";
 
 function App() {
   return (
     <div className={s.container}>
-      <h1 className={s.title}>Phonebook</h1>
+      <h1 className={s.title}>
+        Phonebook
+        <FaPhoneVolume className={s.icon} />
+      </h1>
       <ContactForm />
       <SearchBox />
       <ContactList />
